ec7e5cecf04667ccc802641108936370964550d5
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2024-02-07 12:18:21 +0900

    Refactor: introduce App struct

---
+++ app.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type App struct {
+	config *Configuration
+	logger *log.Logger
+}
+
+func NewApp(args []string) (*App, error) {
+	config, err := NewConfiguration(args)
+	if err != nil {
+		return nil, err
+	}
+
+	logger := log.Default()
+
+	return &App{
+		config: config,
+		logger: logger,
+	}, nil
+}
+
+// Wait for a UDP request on the PalWorld server port.
+// If a request is received, it returns.
+func (app *App) WaitUdpRequest() error {
+	udpAddr, err := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(app.config.PalWorldServerPort))
+	if err != nil {
+		return err
+	}
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	app.logger.Println("Start listening UDP request")
+	var buf [1]byte
+	_, _, err = conn.ReadFromUDP(buf[0:])
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *App) LaunchPalWorldServer(cancel context.CancelFunc) error {
+	cmd := exec.Command(app.config.PalServerCommand[0], app.config.PalServerCommand[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	app.logger.Printf("Executing %s", strings.Join(app.config.PalServerCommand, " "))
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		err := cmd.Wait()
+		if err != nil {
+			app.logger.Printf("An error occurred from the PalServer command: %s\n", err)
+		}
+		cancel()
+	}()
+
+	return nil
+}
+
+var userExistsRe = regexp.MustCompile(`\d+,\d+`)
+
+func (app *App) UserExistenceCheck(ctx context.Context) {
+	go func() {
+		threshold := 30
+		userEmptyCount := 0
+
+		for {
+			time.Sleep(1 * time.Minute)
+
+			select {
+			case <-ctx.Done():
+				app.logger.Println("UserExistenceCheck is shutting down")
+				return
+			default:
+				// do nothing
+			}
+
+			resp, err := RconShowPlayers(app.config)
+			if err != nil {
+				app.logger.Printf("An error occurred while executing ShowPlayers: %s\n", err)
+				continue
+			}
+
+			if userExistsRe.MatchString(resp) {
+				if userEmptyCount > 0 {
+					app.logger.Printf("User exists, resetting userEmptyCount from %d to 0\n", userEmptyCount)
+				}
+				userEmptyCount = 0
+			} else {
+				userEmptyCount++
+				app.logger.Printf("User does not exist, userEmptyCount is increased and now %d\n", userEmptyCount)
+
+				if userEmptyCount >= threshold {
+					app.logger.Println("User does not exist for a while, shutting down PalWorld server")
+					if err := app.ShutdownPalWorldServer(1*time.Second, ""); err != nil {
+						app.logger.Printf("An error occurred while shutting down PalWorld server: %s\n", err)
+					}
+					break
+				}
+			}
+		}
+	}()
+}
+
+func (app *App) MemoryUsageCheck(ctx context.Context, cancel context.CancelFunc) {
+	go func() {
+		for {
+			time.Sleep(1 * time.Minute)
+
+			select {
+			case <-ctx.Done():
+				app.logger.Println("MemoryUsageCheck is shutting down")
+				return
+			default:
+				// do nothing
+			}
+
+			out, err := exec.Command("ps", "-s", strconv.Itoa(os.Getpid()), "-o", "rss").Output()
+			if err != nil {
+				app.logger.Printf("An error occurred while executing ps: %s\n", err)
+				continue
+			}
+
+			mem := 0
+			for _, b := range regexp.MustCompile(`\d+`).FindAll(out, -1) {
+				i, err := strconv.Atoi(string(b))
+				if err != nil {
+					app.logger.Printf("An error occurred while parsing memory usage: %s\n", err)
+					continue
+				}
+				mem += i
+			}
+
+			if mem > app.config.MemoryThreshold {
+				app.logger.Printf("Memory usage exceeds the threshold: %d > %d. Restarting the PalServer in 5 minutes\n", mem, app.config.MemoryThreshold)
+				err := app.ShutdownPalWorldServer(5*time.Minute, "This server will reboot in 5 minutes due to high memory usage. Please save your work.")
+				if err != nil {
+					app.logger.Printf("An error occurred while shutting down PalWorld server: %s\n", err)
+				}
+				break
+			}
+		}
+
+		time.Sleep(4 * time.Minute)
+		app.logger.Println("PalServer will be restarted in 1 minute due to high memory usage")
+		err := RconBroadcast(app.config, "Re-announcement. This server will reboot in 1 minute due to high memory usage. Please save your work.")
+		if err != nil {
+			app.logger.Printf("An error occurred while broadcasting: %s\n", err)
+		}
+	}()
+}
+
+func (app *App) ShutdownPalWorldServer(wait time.Duration, msg string) error {
+	err := RconShutdown(app.config, wait, msg)
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(10 * time.Second)
+	return nil
+}
--- main.go
+++ main.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"context"
-	"log"
 	"os"
-	"os/exec"
-	"regexp"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -19,164 +14,47 @@ func main() {
 }
 
 func Main() error {
-	logger := log.Default()
+
+	app, err := NewApp(os.Args[1:])
+	if err != nil {
+		return err
+	}
+
+	app.logger.Println("Starting Itzpapalotl")
+
+	for {
+		err := inLoop(app)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func inLoop(app *App) error {
 	// This ctx will be done when the PalServer is shutted down
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	config, err := NewConfiguration(os.Args[1:])
+	app.logger.Println("Waiting for UDP request")
+	err := app.WaitUdpRequest()
 	if err != nil {
 		return err
 	}
 
-	logger.Println("Starting Itzpapalotl")
-	logger.Println("Loading configuration")
-
-	for {
-		logger.Println("Waiting for UDP request")
-		err := WaitUdpRequest(config, logger)
-		if err != nil {
-			return err
-		}
-
-		logger.Println("Launching PalWorld server")
-		err = LaunchPalWorldServer(cancel, config, logger)
-		if err != nil {
-			return err
-		}
-
-		time.Sleep(10 * time.Second)
-
-		logger.Println("Waiting for user existence check")
-		UserExistenceCheck(ctx, config, logger)
-		MemoryUsageCheck(ctx, cancel, config, logger)
-
-		<-ctx.Done()
-		logger.Println("PalWorld server is shutted down by some reason. Restarting...")
-	}
-}
-
-func LaunchPalWorldServer(cancel context.CancelFunc, config *Configuration, logger *log.Logger) error {
-	cmd := exec.Command(config.PalServerCommand[0], config.PalServerCommand[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	logger.Printf("Executing %s", strings.Join(config.PalServerCommand, " "))
-	err := cmd.Start()
-	if err != nil {
-		return err
-	}
-
-	go func() {
-		err := cmd.Wait()
-		if err != nil {
-			logger.Printf("An error occurred from the PalServer command: %s\n", err)
-		}
-		cancel()
-	}()
-
-	return nil
-}
-
-var userExistsRe = regexp.MustCompile(`\d+,\d+`)
-
-func UserExistenceCheck(ctx context.Context, config *Configuration, logger *log.Logger) {
-	go func() {
-		threshold := 30
-		userEmptyCount := 0
-
-		for {
-			time.Sleep(1 * time.Minute)
-
-			select {
-			case <-ctx.Done():
-				logger.Println("UserExistenceCheck is shutting down")
-				return
-			default:
-				// do nothing
-			}
-
-			resp, err := RconShowPlayers(config)
-			if err != nil {
-				logger.Printf("An error occurred while executing ShowPlayers: %s\n", err)
-				continue
-			}
-
-			if userExistsRe.MatchString(resp) {
-				if userEmptyCount > 0 {
-					logger.Printf("User exists, resetting userEmptyCount from %d to 0\n", userEmptyCount)
-				}
-				userEmptyCount = 0
-			} else {
-				userEmptyCount++
-				logger.Printf("User does not exist, userEmptyCount is increased and now %d\n", userEmptyCount)
-
-				if userEmptyCount >= threshold {
-					logger.Println("User does not exist for a while, shutting down PalWorld server")
-					if err := ShutdownPalWorldServer(config, logger, 1*time.Second, ""); err != nil {
-						logger.Printf("An error occurred while shutting down PalWorld server: %s\n", err)
-					}
-					break
-				}
-			}
-		}
-	}()
-}
-
-func MemoryUsageCheck(ctx context.Context, cancel context.CancelFunc, config *Configuration, logger *log.Logger) {
-	go func() {
-		for {
-			time.Sleep(1 * time.Minute)
-
-			select {
-			case <-ctx.Done():
-				logger.Println("MemoryUsageCheck is shutting down")
-				return
-			default:
-				// do nothing
-			}
-
-			out, err := exec.Command("ps", "-s", strconv.Itoa(os.Getpid()), "-o", "rss").Output()
-			if err != nil {
-				logger.Printf("An error occurred while executing ps: %s\n", err)
-				continue
-			}
-
-			mem := 0
-			for _, b := range regexp.MustCompile(`\d+`).FindAll(out, -1) {
-				i, err := strconv.Atoi(string(b))
-				if err != nil {
-					logger.Printf("An error occurred while parsing memory usage: %s\n", err)
-					continue
-				}
-				mem += i
-			}
-
-			if mem > config.MemoryThreshold {
-				logger.Printf("Memory usage exceeds the threshold: %d > %d. Restarting the PalServer in 5 minutes\n", mem, config.MemoryThreshold)
-				err := ShutdownPalWorldServer(config, logger, 5*time.Minute, "This server will reboot in 5 minutes due to high memory usage. Please save your work.")
-				if err != nil {
-					logger.Printf("An error occurred while shutting down PalWorld server: %s\n", err)
-				}
-				break
-			}
-		}
-
-		time.Sleep(4 * time.Minute)
-		logger.Println("PalServer will be restarted in 1 minute due to high memory usage")
-		err := RconBroadcast(config, "Re-announcement. This server will reboot in 1 minute due to high memory usage. Please save your work.")
-		if err != nil {
-			logger.Printf("An error occurred while broadcasting: %s\n", err)
-		}
-	}()
-}
-
-func ShutdownPalWorldServer(config *Configuration, logger *log.Logger, wait time.Duration, msg string) error {
-	err := RconShutdown(config, wait, msg)
+	app.logger.Println("Launching PalWorld server")
+	err = app.LaunchPalWorldServer(cancel)
 	if err != nil {
 		return err
 	}
 
 	time.Sleep(10 * time.Second)
+
+	app.logger.Println("Waiting for user existence check")
+	app.UserExistenceCheck(ctx)
+	app.MemoryUsageCheck(ctx, cancel)
+
+	<-ctx.Done()
+	app.logger.Println("PalWorld server is shutted down by some reason. Restarting...")
+
 	return nil
 }
